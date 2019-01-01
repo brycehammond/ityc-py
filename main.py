239a59397e3b58ab03ee63d7552f024f8ee8c6da
Bryce Hammond <bryce@flyreel.co>
2019-01-01 16:40:22 -0700

    Pull from Dynamo for the card

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import boto3
 
 app = Flask(__name__)
 
+# Initialize dynamodb access
+dynamodb = boto3.resource('dynamodb')
+db = dynamodb.Table('ityc')
+
 @app.route("/")
 def home():
-    return render_template('home.html', card = '3', suite = 'd')
+    record = db.get_item(Key={'id': 'card'})['Item']
+    return render_template('home.html', card = record['value'], suit = record['suit'])
 
 if __name__ == "__main__":
     app.run(debug=True)
